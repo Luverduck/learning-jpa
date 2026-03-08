100ec56c7d2678b120fafdb2fde9067a03015611
Luverduck <hyekko92@gmail.com>
2026-03-09 01:20:56 +0900

    05.05. 기본키 매핑

--- jpa-basic/basic/src/main/java/hello/jpa/entity/Member.java
+++ jpa-basic/basic/src/main/java/hello/jpa/entity/Member.java
@@ -5,8 +5,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Transient;
 
 import java.time.LocalDate;
@@ -14,9 +17,14 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Entity
+@SequenceGenerator(name = "MEMBER_SEQ_GENERATOR", sequenceName = "MEMBER_SEQ", initialValue = 1, allocationSize = 5)
+// @TableGenerator(name = "MEMBER_SEQ_GENERATOR_TABLE", pkColumnName = "seq_name", valueColumnName = "seq_next_val", pkColumnValue = "MEMBER_SEQ")
 public class Member {
 
     @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
+    // @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR_TABLE")
     private Long id;
 
     @Column(name = "name", nullable = false)
---
+++ jpa-basic/basic/src/main/java/hello/jpa/mappping/EntityMapping.java
@@ -0,0 +1,39 @@
+package hello.jpa.mappping;
+
+import hello.jpa.entity.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class EntityMapping {
+    public static void main(String[] args) {
+        // EntityManagerFactory 생성
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        // EntityManager 생성
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        // EntityManager를 통해 EntityTransaction 반환
+        EntityTransaction transaction = entityManager.getTransaction();
+        // 트랜잭션 시작
+        transaction.begin();
+        try {
+            // 회원 저장
+            Member member = new Member();
+            member.setName("HelloA");
+            System.out.println("=== EntityManager의 persist() 호출 전 ===");
+            entityManager.persist(member);
+            System.out.println("=== EntityManager의 persist() 호출 후 ===");
+            // 트랜잭션 커밋
+            System.out.println("=== 트랜잭션 커밋 전 ===");
+            transaction.commit();
+            System.out.println("=== 트랜잭션 커밋 후 ===");
+        } catch (Exception e) {
+            // 트랜잭션 롤백
+            transaction.rollback();
+        } finally {
+            // 리소스 정리
+            entityManager.close();
+        }
+        entityManagerFactory.close();
+    }
+}
