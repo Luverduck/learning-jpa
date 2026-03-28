feb6b874cb810d40610f7abc54a9a45beaf8bab2
Luverduck <hyekko92@gmail.com>
2026-03-29 03:13:11 +0900

    09.04. 실전 예제 5 - 연관관계 정리

--- jpa-basic/basic-example/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ jpa-basic/basic-example/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
@@ -20,7 +21,7 @@ public class Delivery extends BaseEntity {
 
     private DeliveryStatus status;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
 
 }
