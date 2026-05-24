b1b0b483fd6c6c5836ab15a8fa9368ef013f69d6
Luverduck <hyekko92@gmail.com>
2026-05-24 15:56:12 +0900

    08.03. 회원 조회

--- spring-boot-jpa-part1/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ spring-boot-jpa-part1/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -37,6 +39,14 @@ public class MemberController {
         memberService.join(member);
         // 홈 화면으로 강제 이동
         return "redirect:/";
+    }
+
+    // 회원 조회
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 
 }
