39c82cd2d869a8cfd43f9d2eedd491383af17daa
Luverduck <hyekko92@gmail.com>
2026-05-29 00:20:06 +0900

    04.01. 간단한 주문 조회 V1 : 엔티티를 직접 반환

--- spring-boot-jpa-part2/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ spring-boot-jpa-part2/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -2,12 +2,23 @@ package jpabook.jpashop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import tools.jackson.datatype.hibernate7.Hibernate7Module;
 
 @SpringBootApplication
 public class JpashopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+	// Hibernate7Module 빈 등록
+	@Bean
+    Hibernate7Module hibernate7Module() {
+		Hibernate7Module hibernate7Module = new Hibernate7Module();
+		// LAZY 프록시 강제 초기화 off
+		hibernate7Module.configure(Hibernate7Module.Feature.FORCE_LAZY_LOADING, false);
+		return hibernate7Module;
 	}
 
 }
---
+++ spring-boot-jpa-part2/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    // 주문 조회 (엔티티 직접 반환)
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        return orders;
+    }
+
+}
