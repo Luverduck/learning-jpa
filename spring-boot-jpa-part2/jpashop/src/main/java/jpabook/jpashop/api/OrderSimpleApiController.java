fc9e2d756ce021b540fa6876b03624555f643c58
Luverduck <hyekko92@gmail.com>
2026-05-29 02:05:54 +0900

    04.02. 간단한 주문 조회 V2 : 엔티티를 DTO로 변환

--- spring-boot-jpa-part2/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ spring-boot-jpa-part2/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,12 +1,16 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -20,6 +24,33 @@ public class OrderSimpleApiController {
     public List<Order> ordersV1() {
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
         return orders;
+    }
+
+    // 주문 조회 (DTO 반환)
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        // 엔티티를 주문 조회 응답 DTO로 변환한 후 반환
+        return orders.stream()
+                        .map(o -> new SimpleOrderDto(o))
+                        .toList();
+    }
+
+    // 주문 조회 응답 DTO
+    @Data
+    static class SimpleOrderDto {
+        private Long id;
+        private String name;
+        private LocalDateTime date;
+        private OrderStatus status;
+        private Address address;
+        public SimpleOrderDto(Order order) {
+            id = order.getId();
+            name = order.getMember().getName(); // LAZY 프록시 초기화
+            date = order.getOrderDate();
+            status = order.getStatus();
+            address = order.getDelivery().getAddress(); // LAZY 프록시 초기화
+        }
     }
 
 }
