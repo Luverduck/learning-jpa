ad232b4dbb281f8ec51b7902349708c5d88714f5
Luverduck <hyekko92@gmail.com>
2026-06-14 19:14:27 +0900

    03.03. 공통 인터페이스 적용

--- spring-data-jpa/basic/src/test/java/springdatajpa/basic/repository/MemberRepositoryTest.java
+++ spring-data-jpa/basic/src/test/java/springdatajpa/basic/repository/MemberRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import springdatajpa.basic.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -24,6 +25,30 @@ public class MemberRepositoryTest {
         Assertions.assertThat(findMember.getId()).isEqualTo(savedMember.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(savedMember.getUsername());
         Assertions.assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    public void basicCRUD() {
+        // 저장 검증
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        // 단일 조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        Assertions.assertThat(findMember1).isEqualTo(member1);
+        Assertions.assertThat(findMember2).isEqualTo(member2);
+        // 전체 조회 검증
+        List<Member> all = memberRepository.findAll();
+        Assertions.assertThat(all).hasSize(2);
+        Long count = memberRepository.count();
+        Assertions.assertThat(count).isEqualTo(2L);
+        // 삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+        Long countAfter = memberRepository.count();
+        Assertions.assertThat(countAfter).isEqualTo(0L);
     }
 
 }
