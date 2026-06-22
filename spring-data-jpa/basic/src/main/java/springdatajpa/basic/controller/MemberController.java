293569d773b0db115bd02393fcd92636979f39aa
Luverduck <hyekko92@gmail.com>
2026-06-23 02:53:58 +0900

    05.04. 웹 확장 - 페이징과 정렬

--- spring-data-jpa/basic/src/main/java/springdatajpa/basic/controller/MemberController.java
+++ spring-data-jpa/basic/src/main/java/springdatajpa/basic/controller/MemberController.java
@@ -2,9 +2,13 @@ package springdatajpa.basic.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import springdatajpa.basic.dto.MemberDto;
 import springdatajpa.basic.entity.Member;
 import springdatajpa.basic.repository.MemberRepository;
 
@@ -25,12 +29,26 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members1")
+    public Page<Member> findMemberList(@PageableDefault(page = 1, size = 5) Pageable pageable) {
+        Page<Member> page =  memberRepository.findAll(pageable);
+        return page;
+    }
+
+    @GetMapping("/members2")
+    public Page<MemberDto> findMemberDtoList(Pageable pageable) {
+        Page<Member> page =  memberRepository.findAll(pageable);
+        Page<MemberDto> result = page.map(member -> new MemberDto(member));
+        return result;
+    }
+
     // MemberController 빈 초기화 후 실행
     @PostConstruct
     public void init() {
-        Member member = new Member("userA");
-        memberRepository.save(member);
-        System.out.println("member.getId() = " + member.getId());
+        for (int i = 0; i < 100; ++i) {
+            Member member = new Member("user" + i);
+            memberRepository.save(member);
+        }
     }
 
 }
