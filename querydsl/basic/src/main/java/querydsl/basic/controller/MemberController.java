a70d4c59634dae30de122794d8a2f44351eaf07b
Luverduck <hyekko92@gmail.com>
2026-08-09 02:09:45 +0900

    06.03. 조회 API 컨트롤러 개발

--- querydsl/basic/src/main/java/querydsl/basic/controller/MemberController.java
+++ querydsl/basic/src/main/java/querydsl/basic/controller/MemberController.java
@@ -1,11 +1,14 @@
 package querydsl.basic.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import querydsl.basic.dto.MemberSearchCondition;
 import querydsl.basic.dto.MemberTeamDto;
 import querydsl.basic.repository.MemberJpaRepository;
+import querydsl.basic.repository.MemberRepository;
 
 import java.util.List;
 
@@ -14,10 +17,16 @@ import java.util.List;
 public class MemberController {
 
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
-    @GetMapping
+    @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJpaRepository.searchByWhereMultiParameter(condition);
+    }
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPage(condition, pageable);
     }
 
 }
